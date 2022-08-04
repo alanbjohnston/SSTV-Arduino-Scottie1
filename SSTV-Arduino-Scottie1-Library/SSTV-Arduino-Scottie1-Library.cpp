db55cc411e763feecac3f931dc4123dfa530f194
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-04 06:10:52 -0400

    Update SSTV-Arduino-Scottie1-Library.cpp

--- SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
+++ SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
@@ -443,7 +443,7 @@ bool get_block(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
   uint16_t *pixel;
   bool last_block = ((x == (320 - w)) & (y == (240 - h)));
   
-  if (((y % h) == 0) && ((x % w) == 0) {
+  if (((y % h) == 0) && ((x % w) == 0)) {
     Serial.print("\nStart of row! x = ");
     Serial.print(x);
     Serial.print(" y = ");
