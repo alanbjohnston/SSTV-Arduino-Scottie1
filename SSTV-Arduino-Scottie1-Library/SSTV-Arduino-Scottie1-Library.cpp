c8caaa3dbbce3b25891e93d946f8c57a541c0ac6
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-06 07:39:11 -0400

    added sstv_stop

--- SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
+++ SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
@@ -459,8 +459,8 @@ void scottie1_transmit_file(char* filename){
 
 //      Serial.println("increment line");
       line++;
-      if(line == 256){
-        Serial.println("Finished");
+      if (line == 256) || sstv_stop) {
+        Serial.println("SSTV Finished");
 //        DDS.setfreq(2, phase);
         dds_setfreq(2);
 //        DDS.down();
