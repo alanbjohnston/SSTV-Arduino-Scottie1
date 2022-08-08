711ee814c079aa7f7abd8ea2ae102ca6d4a60796
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-08 02:05:42 -0400

    Update SSTV-Arduino-Scottie1-Library.cpp

--- SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
+++ SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
@@ -545,7 +545,6 @@ bool get_block(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
   }
 */
   pixel = bitmap;
-  int counter = 0;
   uint32_t total_pixels = w * h;
 
 //  while (total_pixels--) {
