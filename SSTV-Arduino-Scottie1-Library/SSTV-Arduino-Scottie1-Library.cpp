453fa4a81fb9a3008b1c3dec499de417f1be5cd9
alanbjohnston <alan.b.johnston@gmail.com>
2022-10-30 10:00:54 -0400

    print raw and red

--- SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
+++ SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
@@ -709,13 +709,23 @@ bool merged_get_block(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bi
             
           // based on discussion on https://stackoverflow.com/questions/2442576/how-does-one-convert-16-bit-rgb565-to-24-bit-rgb888
           
-//            byte red = pixel_value & 0b1111100000000000;
-//            byte green = pixel_value & 0b0000011111100000;         
-//            byte blue = pixel_value & 0b0000000000011111;         
+            byte red_raw = (pixel_value & 0b1111100000000000) >> 11;
+            byte green_raw = (pixel_value & 0b0000011111100000) >> 5;         
+            byte blue_raw = (pixel_value & 0b0000000000011111;                
+          
             byte red = (((pixel_value & 0b1111100000000000) >> 11) * 255 + 15) / 31;
             byte green = (((pixel_value & 0b0000011111100000) >> 5) * 255 + 31) / 63;
             byte blue = ((pixel_value & 0b0000000000011111) * 255 + 15) / 31;            
-            
+                              
+            print_hex(red_raw);                  
+            print_hex(red);    
+            print_hex(green_raw);                  
+            print_hex(green);   
+            print_hex(blue_raw);                  
+            print_hex(blue);   
+                             
+            Serial.println(" ");
+                             
             JpegDec_sortBuf[(3 * JpegDec_pxSkip) + 0] = red;  // JpegDec_pImg[0];
             JpegDec_sortBuf[(3 * JpegDec_pxSkip) + 1] = green; // JpegDec_pImg[1];
             JpegDec_sortBuf[(3 * JpegDec_pxSkip) + 2] = blue; // JpegDec_pImg[2];
