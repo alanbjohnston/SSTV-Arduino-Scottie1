738b09d5a7cf66b1c2ed3bc9052d4f8871363401
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-03 09:10:43 -0400

    use config.top

--- SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
+++ SSTV-Arduino-Scottie1-Library/SSTV-Arduino-Scottie1-Library.cpp
@@ -146,9 +146,10 @@ void dds_begin() {
     pwm_config_set_clkdiv(&dds_pwm_config, 125.0);  // 1.0f
     pwm_config_set_wrap(&dds_pwm_config, 3);  
     pwm_init(dds_pin_slice, &dds_pwm_config, true);
-    pwm_set_gpio_level(DDS_PWM_PIN, 3);
+    pwm_set_gpio_level(DDS_PWM_PIN, (dds_pwm_config.top + 1) * 0.5);
   
-  if (debug_pwm) {	
+//  if (debug_pwm) 
+  {	
     Serial.print(pwm_gpio_to_slice_num(DDS_PWM_PIN));
     Serial.print(" ");	
     Serial.print(pwm_gpio_to_channel(DDS_PWM_PIN));
